6fdde27c03bab98dc28695b77e0e237e95da2219
mark john rojas <markjohnrojas19@gmail.com>
2026-06-13 17:43:53 +0800

    add getComputerChoice and getHumanChoice functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const zeroToThree = Math.floor(Math.random() * 3);
+
+  if (zeroToThree === 1) {
+    return rock;
+  } else if (zeroToThree === 2) {
+    return paper;
+  } else {
+    return scissors;
+  }
+}
+
+function getHumanChoice() {
+  const humanChoice = prompt(
+    "Choose among these choices: Rock, Paper and Scissors",
+  );
+
+  if (
+    humanChoice === "Rock" ||
+    humanChoice === "rock" ||
+    humanChoice === "ROCK"
+  ) {
+    return rock;
+  } else if (
+    humanChoice === "Paper" ||
+    humanChoice === "paper" ||
+    humanChoice === "PAPER"
+  ) {
+    return paper;
+  } else {
+    return scissors;
+  }
+}
