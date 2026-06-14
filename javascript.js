889e3afc33dbe8d77c81414009a1d1ae9da636c8
mark john rojas <markjohnrojas19@gmail.com>
2026-06-14 14:42:38 +0800

    Complete the game, forgot to commit every progress

--- javascript.js
+++ javascript.js
@@ -38,3 +38,108 @@ function getHumanChoice() {
     return scissors;
   }
 }
+
+function rockBeatsScissors(compick, hupick) {
+  if (compick === "Rock" && hupick === "Scissors") {
+    let message = "You lost! Rock beats Scissors";
+    console.log(message);
+    alert(message);
+    computerScore++;
+    return message;
+  } else if (compick === "Scissors" && hupick === "Rock") {
+    let message = "You win! Rock beats Scissors";
+    console.log(message);
+    alert(message);
+    humanScore++;
+    return message;
+  }
+}
+
+function paperBeatsRock(compick, hupick) {
+  if (compick === "Paper" && hupick === "Rock") {
+    let message = "You lost! Paper beats Rock";
+    console.log(message);
+    computerScore++;
+    alert(message);
+    return message;
+  } else if (compick === "Rock" && hupick === "Paper") {
+    let message = "You win! Paper beats Rock";
+    console.log(message);
+    alert(message);
+    humanScore++;
+    return message;
+  }
+}
+
+function scissorsBeatsPaper(compick, hupick) {
+  if (compick === "Scissors" && hupick === "Paper") {
+    let message = "You lost! Scissors beats Paper";
+    console.log(message);
+    computerScore++;
+    alert(message);
+    return message;
+  } else if (compick === "Paper" && hupick === "Scissors") {
+    let message = "You win! Scissors beats Paper";
+    console.log(message);
+    humanScore++;
+    alert(message);
+    return message;
+  }
+}
+
+function tiedPicked(compick, hupick) {
+  if (compick === hupick) {
+    let message = "Tied! You both picked same.";
+    console.log(message);
+    alert(message);
+    computerScore++;
+    humanScore++;
+    return message;
+  }
+}
+
+function determineTheWinner() {
+  let winnermessage = "";
+  let texthumanscore = String(humanScore);
+  let textcomputerscore = String(computerScore);
+
+  if (humanScore < computerScore) {
+    winnermessage =
+      "You lost! Computer win with scores of " + textcomputerscore;
+    console.log(winnermessage);
+    alert(winnermessage);
+
+    return winnermessage;
+  } else if (humanScore > computerScore) {
+    winnermessage = "You win! Your scores is " + texthumanscore;
+    console.log(winnermessage);
+    alert(winnermessage);
+
+    return winnermessage;
+  } else {
+    winnermessage = "The overall game is a tie!";
+    console.log(winnermessage);
+    alert(winnermessage);
+    return winnermessage;
+  }
+}
+
+function playRound(computerChoice, humanChoice) {
+  rockBeatsScissors(computerChoice, humanChoice);
+  paperBeatsRock(computerChoice, humanChoice);
+  scissorsBeatsPaper(computerChoice, humanChoice);
+  tiedPicked(computerChoice, humanChoice);
+}
+
+function playGame() {
+  for (let i = 1; i <= 5; i++) {
+    const computerSelection = getComputerChoice();
+    const humanSelection = getHumanChoice();
+
+    playRound(computerSelection, humanSelection);
+  }
+
+  determineTheWinner();
+}
+
+playGame();
