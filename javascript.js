4f6fbdc67985cc4c6cf028b98f3e7589569d0c99
mark john rojas <markjohnrojas19@gmail.com>
2026-06-30 20:11:12 +0800

    add html layout

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
+const rock = "./images/rock.png";
+const paper = "./images/paper.png";
+const scissors = "./images/scissor.png";
 
 let humanScore = 0;
 let computerScore = 0;
@@ -18,9 +18,9 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const humanChoice = prompt(
-    "Choose among these choices: Rock, Paper and Scissors",
-  );
+  const humanChoice =
+    prompt();
+    // "Choose among these choices: Rock, Paper and Scissors",
 
   if (
     humanChoice === "Rock" ||
@@ -142,4 +142,4 @@ function playGame() {
   determineTheWinner();
 }
 
-playGame();
+// playGame();
